19e0054f9fb5186b270e8203e4ad82ada88da1ef
JaeKwang Choi <techmate.ai@gmail.com>
2022-05-17 14:49:02 +0900

    nc

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-# hkadfkdflskfs
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+    def __str__(self):
+        return self.title
